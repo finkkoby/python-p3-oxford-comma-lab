398a531c6bdbb6e050431549be71483efc33eb55
Koby Fink <finkkoby@icloud.com>
2024-03-15 18:16:23 -0400

    complete lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,9 @@
 def oxford_comma(items):
-    return None
+    if len(items) > 2:
+        items[-1] = "and " + items[-1]
+        return ", ".join(items)
+    elif len(items) == 2:
+        items.insert(-1, "and")
+        return " ".join(items)
+    else:
+        return "".join(items)
